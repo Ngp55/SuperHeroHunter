25341d765ae9e63dfd69f92d5f5bcf6bea897e3f
MokshDham <asdfrajkumar1122@gmail.com>
2023-05-01 00:41:05 +0530

    favadded

--- script.js
+++ script.js
@@ -1,96 +1,12 @@
-// const allTabsBody = document.querySelectorAll('.tab-body-single');
-// const allTabsHead = document.querySelectorAll('.tab-head-single');
-// const searchForm = document.querySelector('.app-header-search');
-// let searchList = document.getElementById('search-list');
 
-// let activeTab = 1, allData;
-
-// const init = () =>{
-//     showActiveTabBody();
-//     showActiveTabHead();
-// }
-
-// const showActiveTabHead = () => allTabsHead[activeTab - 1].classList.add('active-tab');
-// const showActiveTabBody = () =>{
-//     hideAllTabBody();
-//     allTabsBody[activeTab -1].classList.add('show-tab');
-// }
-// const hideAllTabBody = () => allTabsBody.forEach(singleTabBody=> singleTabBody.classList.remove('show-tab'));
-// const hideAllTabHead = () => allTabsHead.forEach(singleTabHead=> singleTabHead.classList.remove('active-tab'));
-
-
-
-
-// //event listeners
-
-// window.addEventListener('DOMContentLoaded', () => init());
-// // button event listeners
-// allTabsHead.forEach(singleTabHead =>{
-//     singleTabHead.addEventListener('click',() =>{
-//         hideAllTabHead();
-//         activeTab = singleTabHead.dataset.id;
-//         showActiveTabHead();
-//         showActiveTabBody();
-//     });
-// });
-// const getInputValue = (event) => {
-//     event.preventDefault();
-//     let searchText = searchForm.search.value;
-//     fetchAllSuperHero(searchText);
-// }
-
-// // search form submission
-// searchForm.addEventListener('submit',getInputValue);
-
-// const fetchAllSuperHero = async(searchText) => {
-//     let url = `https://www.superheroapi.com/api.php/2319026424925153/search/${searchText}`;
-    
-//     try{
-//         const response = await fetch(url);
-//         allData =await response.json();
-//         if(allData.response === 'sucess'){
-//             //console.log(allData);
-//             showSearchList(allData.results);
-//         }
-
-//     }
-//     catch(error){
-//         console.log(error);
-//     }
-// } 
-
-// const showSearchList = (data) => {
-//     searchList.innerHTML = "";
-//     data.forEach(dataItem => {
-//         const divElem = document.createElement('div');
-//         divElem.classList.add('search-list-item');
-//         divElem.innerHTML = `
-//         <img src = "${dataItem.image.url ? dataItem.image.url : ""}" alt="">
-//         <p data-id ="${dataItem.id}">${dataItem.name}</p>
-//         `;
-//         searchList.appendChild(divElem);
-//     });
-// }
-
-// searchForm.search.addEventListener('keyup', () => {
-//     if(searchForm.search.value.length > 1)
-//             {
-//                 fetchAllSuperHero(searchForm.search.value);
-//             }
-//             else{
-//                 searchList.innerHTML = "";
-//             }
-// });
-
-// searchList.addEventListener('click',(event) =>{
-//     let searchId = event.target.dataset.id;
-//     console.log(searchId);
-// })
 
 const allTabsBody = document.querySelectorAll('.tab-body-single');
 const allTabsHead = document.querySelectorAll('.tab-head-single');
 const searchForm = document.querySelector('.app-header-search');
 let searchList = document.getElementById('search-list');
+
+let favarray = [];
+var favid =0;
 
 let activeTab = 1, allData;
 
@@ -147,6 +63,10 @@ const fetchAllSuperHero = async(searchText) => {
 }
 
 const showSearchList = (data) => {
+    console.log("Searchlist:->"+data[0].id);
+    
+
+
     searchList.innerHTML = "";
     data.forEach(dataItem => {
         const divElem = document.createElement('div');
@@ -177,7 +97,11 @@ searchList.addEventListener('click',(event) =>{
     })
 
     const showSuperHeroDetails = (data) => {
-        console.log(data);
+        console.log("herodetails:->"+data[0].id);
+        var favv= document.getElementById("favbtn");
+        favv.setAttribute("style","display:flex;");
+        favv.setAttribute('value',data[0].id)
+        
         document.querySelector('.app-body-content-thumbnail').innerHTML = `
         <img src = "${data[0].image.url}" alt="image not loaded">
         `;
@@ -343,3 +267,16 @@ searchList.addEventListener('click',(event) =>{
 
 
     }
+
+    // pushing data to favarray and setting it into localstorage.
+function favpush (favid){ 
+    console.log(favid);
+    if (favarray.includes(favid)) {
+      alert("Already Added to the Favourite List");
+      return;
+  }
+    favarray.push(favid);
+   // console.log(data.id + data.name);
+    console.log(favarray);
+    localStorage.setItem('favlistarr', JSON.stringify(favarray));
+  }
